67d72b02d40dbb52c67dab871b3b8915f76667d4
Hao Wu <hawu@pivotal.io>
2019-08-14 17:05:57 +0800

    refactor 8

--- src/containers.c
+++ src/containers.c
@@ -54,9 +54,9 @@ static char *get_coordinator_address(void);
 static int get_new_container_from_coordinator(const char *runtime_id, plcContext *ctx);
 static plcContext *get_new_container_ctx(const char *runtime_id);
 static int init_container_connection(plcContext *ctx);
-static void insert_container_ctx(char *runtime_id, plcContext *ctx, int slot);
+static void insert_container_ctx(const char *runtime_id, plcContext *ctx, int slot);
 
-static void insert_container_ctx(char *runtime_id, plcContext *ctx, int slot)
+static void insert_container_ctx(const char *runtime_id, plcContext *ctx, int slot)
 {
 	containers[slot].runtimeid = plc_top_strdup(runtime_id);
 	containers[slot].ctx = ctx;
@@ -121,7 +121,8 @@ static int get_new_container_from_coordinator(const char *runtime_id, plcContext
 {
 	plcConn *conn; // a connection used to connect to coordinator
 	int ret = 0;
-
+	(void)runtime_id;
+	(void)ctx;
 	conn = (plcConn*) palloc0(sizeof(plcConn));
 
 	/* current only uds is supported */
